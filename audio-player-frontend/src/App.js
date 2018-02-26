b1181526a7e2ca82982084787bdc65f345d2ac97
eyze90 <yu.eugene90@gmail.com>
2018-02-26 15:01:05 -0800

    backend added

--- audio-player-frontend/src/App.js
+++ audio-player-frontend/src/App.js
@@ -1,17 +1,49 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
+import './App.css'
 
 class App extends Component {
+  
   render() {
+
+    const styles={
+      audioPlayer: {
+        position: 'absolute',
+        bottom: 0,
+        height: '30vh',
+        width: '100%'
+      }
+    }
     return (
       <div className="App">
         <header className="App-header">
-          
+          Audio Player
         </header>
-        
+
+        <footer style={styles.audioPlayer} >
+          <AudioPlayer />
+        </footer>
       </div>
     );
   }
 }
+
+class AudioPlayer extends Component {
+
+
+  render() {
+    return (
+      <div>
+        <button onClick={() => this.audioPlayer.play()}>Play</button>
+        <button onClick={() => this.audioPlayer.pause()}>Pause</button>
+        <audio controls ref={(self) => {this.audioPlayer = self}}>
+            <source src="" />
+        </audio>
+      </div>
+    )
+  }
+}
+
+
 
 export default App;
---
+++ audio-player-frontend/src/components/AudioPlayer.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+
+class AudioPlayer extends Component {
+
+
+  render() {
+    return (
+      <audio controls ref={(self) => {this.audioPlayer = self}}>
+          <source src="" />
+      </audio>
+    )
+  }
+}
